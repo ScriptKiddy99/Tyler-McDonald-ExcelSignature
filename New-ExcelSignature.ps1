42911d97bb16554dc4b54db45c008cebba352d94
ScriptKiddy99 <47433174+ScriptKiddy99@users.noreply.github.com>
2019-02-07 21:42:11 +0000

    Initial Version
    This should resolve your issue, the reason your code was not working is because you kept getting that annoying signature setup prompt, this can be resolved by setting the Display Alerts property of the Excel object to false like so ;$Excel.DisplayAlerts = $False
    
    This will associate some default behaviour to our excel instance so be careful to either re-enable this if you require any prompts etc. such as those that are produced by the Quit() method etc.

---
+++ New-ExcelSignature.ps1
@@ -0,0 +1,16 @@
+﻿# Creates a new Excel Com Object.
+$Excel = new-object -comobject excel.application 
+ 
+# Set window view size if Excel opens
+$Excel.WindowState = "xlMaximized"
+# Sets the window vsilbility property to true so we can see the excel instance.
+$Excel.visible = $true
+
+# Stops all those annoying alerts and pops ups from getting in our way.
+$Excel.DisplayAlerts = $false
+
+# Create New Workbook.
+$workbook = $Excel.Workbooks.Add() 
+
+# Adds a new singature to the work book.
+$workbook.Signatures.AddSignatureLine("{00000000-0000-0000-0000-000000000000}")
