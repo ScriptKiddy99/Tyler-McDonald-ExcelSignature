964f4a79ba45c6295e2a2eb0d056cae93954afd3
ScriptKiddy99 <47433174+ScriptKiddy99@users.noreply.github.com>
2019-02-09 21:05:31 +0000

    Custom signature location
    "any ideas on setting position. I have it working right now but have to create it as a macro and then call it. It works but I assume PS surely can do it with less code."
    
    Solution, choose a cell location for the signature to be placed, call the .Select() method on our cell range then insert the singature.

--- New-ExcelSignature.ps1
+++ New-ExcelSignature.ps1
@@ -1,4 +1,4 @@
-﻿# Creates a new Excel Com Object.
+# Creates a new Excel Com Object.
 $Excel = new-object -comobject excel.application 
  
 # Set window view size if Excel opens
@@ -12,5 +12,15 @@ $Excel.DisplayAlerts = $false
 # Create New Workbook.
 $workbook = $Excel.Workbooks.Add() 
 
-# Adds a new singature to the work book.
+# Gets the first Woorksheet in the currently open workbook.
+$WorkSheet_Sheet1 = $workbook.Worksheets.item(1)
+
+# stores a range of cells (In this case we select just G2@
+$range = $WorkSheet_Sheet1.Range("G2")
+
+# Selects our chosen range
+$range.Select()
+
+# Adds a new singature to the work book. with the top left of the singaure being at G2
 $workbook.Signatures.AddSignatureLine("{00000000-0000-0000-0000-000000000000}")
+
